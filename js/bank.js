beede40145a2383558a879068775f12263d26a6b
angho86 <myboks123@gmail.com>
2024-10-31 17:24:46 +0200

    added deposit method

--- js/bank.js
+++ js/bank.js
@@ -1,19 +1,18 @@
 class bankAccount{
-    #balance;
+    #balance = 0;
     #deposit;
     #withdraw;
     #history;
 
-    constructor(income, withdraw){
+    constructor(income){
         if(typeof income ==='number' && !isNaN(income)){
             this.#deposit = income;
-            this.#withdraw = withdraw;
-            this.#balance = this.#balance + this.#deposit;
+            this.#balance += this.#deposit;
             this.#history = [];
         } else {
             console.log("Value you try to enter must be a number");
         }
-    };
+    }
 
     get balance(){
         return this.#balance;
@@ -22,8 +21,34 @@ class bankAccount{
     get history(){
         return this.#history;
     }
+
+    get deposit(){
+        return this.#deposit;
+    }
+
+    set deposit(newDeposit) {
+        if(typeof newDeposit ==='number' && !isNaN(newDeposit)){
+            this.#deposit = newDeposit;
+            this.#balance += this.#deposit;
+            const date = new Date();
+            const time = `${date.getFullYear()}/${date.getMonth()}/${date.getDate()} - ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+            const historyObj = {
+                deposited: this.#deposit,
+                time: time,
+            };
+
+            this.#history.push(historyObj);
+        } else {
+            return 'Please enter a number';
+        }
+    }
 }
 
 const bal = new bankAccount(10);
 
+console.log(bal.balance);
+
+bal.deposit = 20;
+
+console.log(bal.history);
 console.log(bal.balance);
